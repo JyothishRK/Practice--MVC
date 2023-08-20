07f51aad999a2081331d1ac658502a5b4d589765
R.K.Jyothish <112569842+JyothishRK@users.noreply.github.com>
2023-08-20 23:43:18 +0530

    added student.php and made changes in core folder

--- task1/app/controller/Home.php
+++ task1/app/controller/Home.php
@@ -6,9 +6,5 @@ class Home extends Controller
     {
         echo "This is home Controller";
         $this->view('home');
-        $model = new Model();
-        $arr['Name']='Peter';
-        $arr['Marks']=45;
-        $model->delete(4); 
     }
 }
